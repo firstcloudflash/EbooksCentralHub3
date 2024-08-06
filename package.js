53b19d614f8d5d94378f2d91840a61e4ac785393
firstcloudflash <firstcloudflash@gmail.com>
2024-08-06 15:21:03 +0100

    Create package.js
    Package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "EbooksCentralHub",
+  "version": "1.0.0",
+  "description": "A comprehensive platform for ebooks, blogs, and more.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "body-parser": "^1.19.0",
+    "stripe": "^8.0.0"
+  },
+  "author": "Your Name",
+  "license": "ISC"
+}
